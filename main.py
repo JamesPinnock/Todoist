bf675c70c3aac396c4002d2a403c241f9ac4f17f
James <james-pinnock@hotmail.com>
2021-12-15 19:42:14 +0000

    main.py file to run the commands (pipeline)

---
+++ main.py
@@ -0,0 +1,26 @@
+from api_key import api
+from backup_functions import important, important_urgent, update_items_with_label_and_priorty, urgent,print_all, update_items_with_due_date,get_all_priority
+api.sync()
+
+##########
+# important()
+# important_urgent()
+
+"""
+The order matters annoyingly 
+"""
+# update_items_with_label_and_priorty(urgent(), 2)
+# update_items_with_label_and_priorty(important(), 3)
+# update_items_with_label_and_priorty(important_urgent(), 4)
+# update_items_with_due_date(get_all_priority())
+
+# update_items([5357142685, 5358150795])
+# [5357142685, 5358150795]
+# print_all()
+
+# if label == urgent:
+    # then mark with piriorty
+# Urgent and important p1
+# def Important & not urgent p2
+# Urgent & unimportant p3
+# Unimportant & Not urgent p4
