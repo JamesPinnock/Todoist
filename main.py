c9347713f0aa3ae56849b415d170a9e042437e44
James <james-pinnock@outlook.com>
2021-12-26 08:30:55 +0000

    Testing if main is running

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ api.sync()
 """
 The order matters annoyingly 
 """
-update_items_with_label_and_priorty(urgent(), 2)
-update_items_with_label_and_priorty(important(), 3)
-update_items_with_label_and_priorty(important_urgent(), 4)
+print("main.py running")
+# update_items_with_label_and_priorty(urgent(), 2)
+# update_items_with_label_and_priorty(important(), 3)
+# update_items_with_label_and_priorty(important_urgent(), 4)
 # update_items_with_due_date(get_all_priority())
 
 # update_items([5357142685, 5358150795])
